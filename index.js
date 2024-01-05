cc459a152cf2d9be28bceba178eec8a507d686a4
madhushreesk <88674169+madhushreesk@users.noreply.github.com>
2024-01-05 16:27:41 +0530

    add patient from BE completed

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
 const {
   getGoogleSheet,
   addRowToSheet,
@@ -9,75 +11,89 @@ const {
 } = require("./Drive");
 
 const app = express();
-
-const main = async () => {
-  try {
-    const sheetName = "appointment";
-    const sheet = await getGoogleSheet(sheetName);
-
-    await addRowToSheet(sheet, {
-      AppointmentID: "123",
-      PatientID: "s v road belgaum",
-      PhysicianID: "belgaum",
-      visit_date: 22,
-      next_date: "female",
-    });
-  } catch (error) {
-    console.error("An error occurred:", error.message);
-  }
-
-  try {
-    const sheetName = "patient";
-    const sheet = await getGoogleSheet(sheetName);
-    await addRowToSheet(sheet, {
-      name: "Madhushri kulkarni",
-      Address: "s v road belgaum",
-      Location: "belgaum",
-      Age: 22,
-      Gender: "female",
-      Phone: "12345",
-    });
-  } catch (error) {
-    console.log("error occured in main");
-    console.error("An error occurred:", error.message);
-  }
-
-  try {
-    const sheetName = "patient";
-    const sheet = await getGoogleSheet(sheetName);
-    await addRowToSheet(sheet, {
-      name: "Madhushri kulkarni",
-      Address: "s v road belgaum",
-      Location: "belgaum",
-      Age: 22,
-      Gender: "female",
-      Phone: "12345",
-    });
-  } catch (error) {
-    console.log("error occured in main");
-    console.error("An error occurred:", error.message);
-  }
-
-  try {
-    const sheetName = "patient";
-    const sheet = await getGoogleSheet(sheetName);
-    await addRowToSheet(sheet, {
-      name: "Madhushri kulkarni",
-      Address: "s v road belgaum",
-      Location: "belgaum",
-      Age: 22,
-      Gender: "female",
-      Phone: "12345",
-    });
-  } catch (error) {
-    console.log("error occured in main");
-    console.error("An error occurred:", error.message);
-  }
-};
-
-main();
-
 app.use(express.json());
+
+app.use(cors());
+app.use(bodyParser.json());
+
+app.post("/dashboard/addPatients", (req, res) => {
+  const {
+    physicianID,
+    patientID,
+    patientName,
+    location,
+    gender,
+    address,
+    phone,
+    age,
+    prescription,
+    dose,
+    visit_date,
+    next_visit,
+    bill,
+    physicianName,
+    physicianNumber,
+  } = req.body;
+
+  const addPatient = async () => {
+    try {
+      const sheetName = "appointment";
+      const sheet = await getGoogleSheet(sheetName);
+
+      await addRowToSheet(sheet, {
+        appointmentID: "123",
+        patientID: patientID,
+        physicianID: physicianID,
+        visit_date: visit_date,
+        next_visit: next_visit,
+      });
+    } catch (error) {
+      console.error("An error occurred:", error.message);
+    }
+    try {
+      const sheetName = "prescribes";
+      const sheet = await getGoogleSheet(sheetName);
+      await addRowToSheet(sheet, {
+        physicianID: physicianID,
+        patientID: patientID,
+        prescription: prescription,
+        dose: dose,
+        bill: bill,
+      });
+    } catch (error) {
+      console.error("An error occurred:", error.message);
+    }
+
+    try {
+      const sheetName = "patient";
+      const sheet = await getGoogleSheet(sheetName);
+      await addRowToSheet(sheet, {
+        patientName: patientName,
+        address: address,
+        location: location,
+        age: age,
+        gender: gender,
+        phone: phone,
+      });
+    } catch (error) {
+      console.error("An error occurred:", error.message);
+    }
+
+    try {
+      const sheetName = "physician";
+      const sheet = await getGoogleSheet(sheetName);
+      await addRowToSheet(sheet, {
+        physicianID: physicianID,
+        physicianName: physicianName,
+        physicianNumber: physicianNumber,
+      });
+    } catch (error) {
+      console.error("An error occurred:", error.message);
+    }
+    res.status(200).send("Success");
+  };
+  addPatient();
+});
 
 const port = 8080;
 
